84f4a9360a5134c34c7cfeef5337a83ff8e76858
Leandr022 <leandro.santagada@hotmail.com>
2023-08-07 13:25:38 -0300

    se agrego alerts y prompts

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-//alert ()
-
 let vidaHarry = 250;
 let vidaDraco = 250;
 
@@ -17,41 +15,48 @@ function harryVive(){
     return vidaHarry > 0
 }
 
+prompt("¡Bienvenido a la batalla de hechizos entre Harry Potter y Draco Malfoy! Presiona 'Aceptar' para empezar.");
+
 
 while(sigueLucha()){ 
     round += 1;
     
+    let hechizoUsuario = prompt(`¡Ronda ${round}! Elige tu hechizo (Expelliarmus / Sectumsempra / Avada Kedavra):`);
     let echizoHarry = calcularHechizo()
     let echizoDraco = calcularHechizo()
     
-    if(echizoHarry === echizoDraco){
-        vidaDraco -= echizoHarry;
-        vidaHarry-= echizoDraco;
-    }else if(echizoHarry>echizoDraco){
-        vidaDraco -=echizoHarry;
-        document.write('<img src="imagenes/harry.png" alt="HechizoHarry" width= "150" />') 
-    }else{
-        vidaHarry-= echizoDraco;
-        document.write('<img src="imagenes/draco.png" alt="HechizoDraco" width= "150" />') 
+
+    if (hechizoUsuario === "Expelliarmus" || hechizoUsuario === "Sectumsempra" || hechizoUsuario === "Avada Kedavra") {
+        if(echizoHarry === echizoDraco){
+            vidaDraco -= echizoHarry;
+            vidaHarry -= echizoDraco;
+        }else if(echizoHarry > echizoDraco){
+            vidaDraco -= echizoHarry;
+            alert("¡Harry conjuró un hechizo más poderoso! ¡Daño a Draco!");
+            document.body.innerHTML += '<img src="imagenes/harry.png" alt="HechizoHarry" width="150" />';
+        }else{
+            vidaHarry -= echizoDraco;
+            alert("¡Draco lanzó un hechizo más poderoso! ¡Daño a Harry!");
+            document.body.innerHTML += '<img src="imagenes/draco.png" alt="HechizoDraco" width="150" />';
+        }
+    } else {
+        alert("¡Hechizo no válido! Harry se distrajo y Draco aprovechó para atacar.");
+        vidaHarry -= echizoDraco;
+        document.body.innerHTML += '<img src="imagenes/draco.png" alt="HechizoDraco" width="150" />';
     }
     
-    
-    
-    
-    console.log("-----------------")
-    console.log("--- Round " + round + " ---")
-    
-    
-    console.log("Vida de Harry Potter " + vidaHarry)
-    console.log("Vida de Draco Malfoy " + vidaDraco)
+    console.log("-----------------");
+    console.log("--- Round " + round + " ---");
+    console.log("Vida de Harry Potter: " + vidaHarry);
+    console.log("Vida de Draco Malfoy: " + vidaDraco);
 }
 
-console.log("-----------------")
+console.log("-----------------");
 
 if(harryVive()){
-    console.log("VICTORIA HARRY POTTER")
-    document.write('<img src="imagenes/gryffindor.png" alt="Gryffindor gana" width= "200" />')
+    alert("¡VICTORIA DE HARRY POTTER!");
+    document.body.innerHTML += '<img src="imagenes/gryffindor.png" alt="Gryffindor gana" width="200" />';
 }else{
-    console.log("VICTORIA DRACO MALFOY")
-    document.write('<img src="imagenes/slytherin.png" alt="Slytherin gana" width= "200" />')
+    alert("¡VICTORIA DE DRACO MALFOY!");
+    document.body.innerHTML += '<img src="imagenes/slytherin.png" alt="Slytherin gana" width="200" />';
 }
